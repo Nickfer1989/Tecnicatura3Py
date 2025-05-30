119f3250a2345ba1031ea6ababe872c3c640ddb7
Nicolás Fernández <nicolaz_alvert@hotmail.com>
2025-05-29 22:30:22 -0300

    Subida Clase 6 Transacciones en Postgresql y Python

---
+++ Leccion06/transacciones_dos.py
@@ -0,0 +1,21 @@
+import psycopg2 as bd # Esto es para poder conectarnos a Postgres
+
+conexion = bd.connect(user= 'postgres',password='admin',host='127.0.0.1',port='5432',database='test_bd')
+try:
+    conexion.autocommit = False #Esto directamente no  deberia estar,  inicia la transacción
+    cursor = conexion.cursor()
+    sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
+    valores = ('Jorge', 'Prol45678912', 'jprol@mail.com')
+    cursor.execute(sentencia, valores)
+
+    sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    valores = ('Juan Carlos', 'Perez', 'jcperez@mail.com', 1)
+    cursor.execute(sentencia, valores)
+
+    conexion.commit()  # Hacemos el commit manualmente, se cierra la transacción
+    print('Termina la transacción')
+except Exception as e:
+    conexion.rollback()
+    print(f'Ocurrió un error, se hizo un rollback: {e}')
+finally:
+    conexion.close()
